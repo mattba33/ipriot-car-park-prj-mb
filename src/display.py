112d3af6d930573109af6af502982d7c82b7772b
20154023 <20154023@tafe.wa.edu.au>
2025-11-19 13:43:31 +0800

    feat: Added methods to car park and display classes fix: Cleaned up return strings in sensor

--- src/display.py
+++ src/display.py
@@ -4,5 +4,9 @@ class Display:
         self.message = message
         self.is_on = is_on
 
+    def update(self, data):
+        for key, value in data.items():
+            print(f"{key}: {value}")
+
     def __str__(self):
         return f"Display {self.id}: {self.message}"
--- src/sensor.py
+++ src/sensor.py
@@ -1,15 +1,23 @@
 class Sensor:
-    def __init__(self, id, is_active=False, car_park):
+    def __init__(self, id, car_park, is_active=False):
         self.id = id
         self.is_active = is_active
         self.car_park = car_park
 
     def __str__(self):
-        return f"Sensor {self.id}:active = {self.is_active} car park={self.car_park}"
+        return (f"Sensor {self.id}:active = {self.is_active} "
+                f"car park={self.car_park}")
+
 
 class EntrySensor(Sensor):
     def __str__(self):
-        return f"EntrySensor {self.id}, active={self.is_active}, car park={self.car_park})"
+        return (f"EntrySensor {self.id}, "
+                f"active={self.is_active}, "
+                f"car park={self.car_park})")
+
+
 class ExitSensor(Sensor):
     def __str__(self):
-        return f"ExitSensor {self.id}, active={self.is_active}, car park={self.car_park})"
+        return (f"ExitSensor {self.id}, "
+                f"active={self.is_active}, "
+                f"car park={self.car_park})")
