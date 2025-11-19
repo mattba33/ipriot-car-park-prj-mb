adbc9b803750311facbdb0a214645564cc322759
20154023 <20154023@tafe.wa.edu.au>
2025-11-19 11:52:14 +0800

    feat: added constructor and attributes to car park class

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,12 @@
 class Carpark:
-    pass
+    def __init__(self, location, capcity, plates=None, displays=None):
+        self.location = location
+        self.capacity = capcity
+        self.displays = displays if displays is not None else []
+        self.plates = plates if plates is not None else []
+
+    def __str__(self):
+        return (f"Car park location: {self.location!r}, "
+                f"capacity: {self.capacity}, "
+                f"plates: {self.plates}, "
+                f"displays: {self.displays})")
