2d804844a6b04d7c6e62746c1c650650e42f19b6
20154023 <20154023@tafe.wa.edu.au>
2025-11-19 13:43:31 +0800

    feat: Added methods to car park and display classes fix: Cleaned up return strings in sensor

--- src/car_park.py
+++ src/car_park.py
@@ -2,7 +2,7 @@ from sensor import Sensor
 from display import Display
 
 
-class Carpark:
+class CarPark:
     def __init__(self, location, capacity, plates=None, displays=None, sensors=None):
         self.location = location
         self.capacity = capacity
@@ -17,6 +17,28 @@ class Carpark:
             self.sensors.append(component)
         else:
             self.displays.append(component)
+
+    def update_displays(self):
+        data = {"available_bays": self.available_bays(),
+                "Location": self.location,
+                "temperature": 25
+                }
+        for display in self.displays:
+            display.update(data)
+
+    def add_car(self, plate):
+        self.plates.append(plate)
+        self.update_displays()
+
+    def remove_car(self, plate):
+        self.plates.remove(plate)
+        self.update_displays()
+
+    def available_bays(self):
+        if self.capacity < len(self.plates):
+            return 0
+        else:
+            return self.capacity - len(self.plates)
 
     def __str__(self):
         return (f"Car park location: {self.location!r}, "
