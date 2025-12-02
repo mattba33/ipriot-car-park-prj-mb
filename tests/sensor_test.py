894d90b7b643d13cb6aa552b27e8392ba00b03e2
20154023 <20154023@tafe.wa.edu.au>
2025-12-02 22:53:55 +0800

    feat: Added sensor_test and added type error test for test_car_park

---
+++ tests/sensor_test.py
@@ -0,0 +1,36 @@
+import unittest
+from src.car_park import CarPark
+from src.sensor import EntrySensor, ExitSensor
+
+
+class SensorTest(unittest.TestCase):
+    def setUp(self):
+        self.car_park = CarPark(
+            location="Perth",
+            capacity=100
+        )
+        self.entry_sensor = EntrySensor(
+            id=1,
+            is_active=True,
+            car_park=self.car_park
+        )
+        self.exit_sensor = ExitSensor(
+            id=2,
+            is_active=True,
+            car_park=self.car_park
+        )
+
+    def test_entry_sensor(self):
+        initial_bays = self.car_park.available_bays
+        self.entry_sensor.detect_vehicle()
+        self.assertEqual(self.car_park.available_bays, initial_bays - 1)
+        self.assertEqual(len(self.car_park.plates), 1)
+        plate = self.car_park.plates[0]
+        self.assertTrue(plate.startswith("FAKE-"))
+
+    def test_exit_sensor(self):
+        self.car_park.add_car("FAKE-001")
+        initial_bays = self.car_park.available_bays
+        self.exit_sensor.detect_vehicle()
+        self.assertEqual(self.car_park.available_bays, initial_bays + 1)
+        self.assertNotIn("FAKE-001", self.car_park.plates)
