f4b417e630098baeff11e19b0ac0e10e0cf956de
20154023 <20154023@tafe.wa.edu.au>
2025-12-03 14:54:39 +0800

    feat: Added pathlib, file reading, car logging to car_park, added unt tests for car park activity chore: Made code more inline with PEP8

--- tests/test_car_park.py
+++ tests/test_car_park.py
@@ -1,9 +1,16 @@
 import unittest
 from src.car_park import CarPark
+from pathlib import Path
+
 
 class TestCarPark(unittest.TestCase):
     def setUp(self):
         self.car_park = CarPark("123 Example Street", 100)
+
+    def tearDown(self):
+        Path("new_log.txt").unlink(missing_ok=True)
+        Path("custom_log.txt").unlink(missing_ok=True)
+        Path("log.txt").unlink(missing_ok=True)
 
     def test_car_park_initialized_with_all_attributes(self):
         self.assertIsInstance(self.car_park, CarPark)
@@ -12,6 +19,42 @@ class TestCarPark(unittest.TestCase):
         self.assertEqual(self.car_park.plates, [])
         self.assertEqual(self.car_park.displays, [])
         self.assertEqual(self.car_park.available_bays, 100)
+
+        self.assertTrue(hasattr(self.car_park, "log_file"))
+        self.assertEqual(self.car_park.log_file, Path("log.txt"))
+
+        custom_log = Path("custom_log.txt")
+        car_park = CarPark("123 Example Street", 100, log_file=custom_log)
+        self.assertEqual(car_park.log_file, custom_log)
+
+    def test_log_file_created(self):
+        CarPark("123 Example Street", 100, log_file="new_log.txt")
+        self.assertTrue(Path("new_log.txt").exists())
+
+    def test_car_logged_when_entering(self):
+        carpark = CarPark("123 Example Street", 100, log_file="new_log.txt")
+        carpark.log_file.write_text("")
+        carpark.add_car("NEW-001")
+
+        with carpark.log_file.open() as file:
+            last_line = file.readlines()[-1]
+
+        self.assertIn("NEW-001", last_line)
+        self.assertIn("entered", last_line)
+        self.assertTrue(last_line.endswith("\n"))
+
+    def test_car_logged_when_exiting(self):
+        carpark = CarPark("123 Example Street", 100, log_file="new_log.txt")
+        carpark.log_file.write_text("")
+        carpark.add_car("NEW-001")
+        carpark.remove_car("NEW-001")
+
+        with carpark.log_file.open() as file:
+            last_line = file.readlines()[-1]
+
+        self.assertIn("NEW-001", last_line)
+        self.assertIn("exited", last_line)
+        self.assertTrue(last_line.endswith("\n"))
 
     def test_add_car(self):
         self.car_park.add_car("FAKE-001")
@@ -41,11 +84,11 @@ class TestCarPark(unittest.TestCase):
             self.car_park.remove_car("NO-1")
 
     def test_register_raises_type_error(self):
-        car_park = CarPark(location="Perth", capacity=100)
         invalid_input = "NotACarParkObject"
 
         with self.assertRaises(TypeError):
             self.car_park.register(invalid_input)
+
 
 if __name__ == "__main__":
     unittest.main()
